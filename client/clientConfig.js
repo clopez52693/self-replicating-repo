df6cb4d4c0971745255d8dbf0820539fe083cc42
clopez52693 <clopez52693@gmail.com>
2018-11-04 12:46:23 -0500

    Committing file 15 of 37: client/clientConfig.js

---
+++ client/clientConfig.js
@@ -0,0 +1,10 @@
+export const clientConfig = {
+  app: {
+    defaultRepoName: 'self-replicating-repo',
+  },
+  github: {
+    clientId: 'a2095cdeb6ef908ffafb',
+    redirectUri: 'https://nf-self-replicating-repo.herokuapp.com/login',
+    defaultScope: 'user public_repo',
+  },
+};
